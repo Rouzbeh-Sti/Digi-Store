7f3b41e2c9de780715f038b113ece2a4c7880aca
erfanPnj <erfanpanjehshahi@gmail.com>
2026-07-17 19:33:01 +0330

    feat: Implement digiCourse logic for buyer

--- backend/routes/buyerRoutes.js
+++ backend/routes/buyerRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getBuyerDashboard, downloadSecureProduct } = require('../controllers/buyerController');
+const { getBuyerDashboard, downloadSecureProduct, accessDigiCourseProduct } = require('../controllers/buyerController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.use(verifyToken);
 
 router.get('/dashboard', getBuyerDashboard);
 router.get('/download/:licenseKey', downloadSecureProduct);
+router.get('/digicourse/:productId', accessDigiCourseProduct);
 
 module.exports = router;
